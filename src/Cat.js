b737cd72a3bc066c9740c615d2b6476ce710b533
Alen Sendro <sendro.alen@nsoft.ba>
2019-05-25 05:00:26 +0200

    Add loading state

--- src/Cat.js
+++ src/Cat.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Cat({ imgSrc, getRandomCatImage }) {
+function Cat({ imgSrc, getRandomCatImage, loading }) {
   return (
     <div className="cat">
       <div>
-          <button className="cat-btn" onClick={getRandomCatImage}>Get random cat image</button>
+          <button className="cat-btn" onClick={getRandomCatImage}>{loading ? 'Loading...' : 'Get random cat image'}</button>
       </div>
       {imgSrc && <img className="cat-img" src={imgSrc} alt="Random cat" />}
     </div>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import Cat from './Cat';
 import './cat.css';
 
 let getRandomCatImage = () => {
+    props.loading = true;
+    render();
     const api = 'https://aws.random.cat/meow';
     return fetch(api).then(response => {
         const promise = response.json();
 
         promise.then((data) => {
             props.imgSrc = data.file;
+            props.loading = false;
             render();
         });
         return response;
@@ -18,7 +21,8 @@ let getRandomCatImage = () => {
 
 let props = {
     imgSrc: '',
-    getRandomCatImage
+    getRandomCatImage,
+    loading: false
 };
 
 const CatWrapper = () => {
